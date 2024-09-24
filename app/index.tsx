8876daa1334b77dd5d13b579b8995c9dff128a09
Lance <85811392+Lance-Town@users.noreply.github.com>
2024-09-23 17:29:11 -0700

    add person FlatList
    - Simple list of different temporary people

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={[
+          {key: 'Person 1'},
+          {key: 'Person 2'},
+          {key: 'Person 3'},
+          {key: 'Person 4'},
+          {key: 'Person 5'},
+          {key: 'Person 6'},
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
     </View>
   );
 }
