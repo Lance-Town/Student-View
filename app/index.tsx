21fe56576fad968d6e715cdbb619e682b2ed6a38
Lance <85811392+Lance-Town@users.noreply.github.com>
2024-09-26 18:50:06 -0700

    add links to students
    - Click on each students name to see more information about them in a new view

--- app/index.tsx
+++ app/index.tsx
@@ -1,49 +1,58 @@
 import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 22,
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
+    container: {
+        flex: 1,
+        paddingTop: 22,
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
 });
 
-// Define the Student interface
 interface Student {
-  id: number;
-  name: string;
-  college: string;
-  state: string;
-  interest: string;
+    id: number;
+    name: string;
+    college: string;
+    state: string;
+    interest: string;
 }
 
 export default function Index() {
-  // Set the initial state
-  const [students, setStudents] = useState<Student[]>([]);
+    const [students, setStudents] = useState<Student[]>([]);
 
-  useEffect(() => {
-    fetch('http://localhost:3000/students')
-    .then((response) => response.json())
-    .then((data: Student[]) => {
-      setStudents(data);
-    })
-    .catch((error) => {
-      console.error('Error fetching data:', error);
-    });
-  }, []);
+    useEffect(() => {
+        fetch("http://localhost:3000/students")
+            .then((response) => response.json())
+            .then((data: Student[]) => {
+                setStudents(data);
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+    }, []);
 
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={students}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
-      />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={students}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                    <Link
+                        href={{
+                            pathname: `./${item.name}`,
+                            params: { user: item.name },
+                        }}
+                        style={styles.item}
+                    >
+                        {item.name}
+                    </Link>
+                )}
+            />
+        </View>
+    );
 }
